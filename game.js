992bb04298dd2c60ecff5a93276409075a64c203
cht2006ky <cht2006ky@163.com>
2026-07-24 21:36:26 +0800

    CHT参数再次修改，2026年7月24日，21点24分

--- game.js
+++ game.js
@@ -43,10 +43,10 @@
     const DY = [-1, 0, 1, 0];
 
     // 游戏参数
-    const PLAYER_SPEED = 2;
-    const ENEMY_SPEED = 1;
-    const BULLET_SPEED = 4;
-    const MAX_ENEMIES = 15;        // 同时在场最大敌人数
+    const PLAYER_SPEED = 5;
+    const ENEMY_SPEED = 5;
+    const BULLET_SPEED = 8;
+    const MAX_ENEMIES = 12;        // 同时在场最大敌人数
     const TOTAL_ENEMIES = 30;     // 本关总敌人数
     const PLAYER_LIVES = 3;
     const SHOOT_COOLDOWN = 20;    // 射击冷却帧数
